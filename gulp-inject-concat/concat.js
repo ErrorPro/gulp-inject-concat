475120d4c369df401dea9b9901a4d1b830f1a648
Ven <mohtaiiihuk@gmail.com>
2015-07-20 21:00:17 +0400

    WIP

--- gulp-inject-concat/concat.js
+++ gulp-inject-concat/concat.js
@@ -1,6 +1,6 @@
 var fs = require('mz/fs');
 var co = require('co');
-var tags = require('./tags')
+var tags = require('./tags');
 var Concat = require('concat-with-sourcemaps');
 
 module.exports = co.wrap(function* (text, file, sourcemaps){
@@ -11,8 +11,8 @@ module.exports = co.wrap(function* (text, file, sourcemaps){
 
   while(res = regexp.exec(text)){
     var filename = res[1].replace(/^\//, '');
-    concat.add(filename, yield fs.readFile(filename))
+    concat.add(filename, yield fs.readFile(filename));
   }
 
-  yield fs.writeFileSync(file, concat.content)
+  yield fs.writeFileSync(file, concat.content);
 })
--- gulp-inject-concat/gulp-inject-concat.js
+++ gulp-inject-concat/gulp-inject-concat.js
@@ -5,20 +5,20 @@ var tags = require('./tags');
 module.exports = function(file, opt){
   if (!file) {
     throw new Error('Missing file option for gulp-inject-concat');
-  }
+  };
   opt = opt || {};
 
-  function bufferContents (chunk, enc, callback){
+  function bufferContents(chunk, enc, callback) {
     var html = chunk.contents.toString(enc);
 
-    concatFile(html, file, opt.sourcemaps)
+    concatFile(html, file, opt.sourcemaps);
 
     chunk.contents =  new Buffer(html.replace(tags.replace, "<!--$&-->")
         .replace(/(.+?)<!-- endconcat -->/gi,
           '$1<script src="'+file+'"><\/script>\n$&'), enc);
 
-    this.push(chunk)
-    callback()
+    this.push(chunk);
+    callback();
   }
 
   return through.obj(bufferContents);
